f595b406e6e4cc3037bf9faddab4158ecf9aebf0
Robinson Regalado <rregalado17@gmail.com>
2019-10-02 23:49:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_233917) do
-
-  create_table "dogs", force: :cascade do |t|
-    t.string "name"
-    t.string "breed"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
